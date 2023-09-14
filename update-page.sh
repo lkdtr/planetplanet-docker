644ed6da8db9a9486fe09156390c95fac14546a2
Bahri Meriç Canlı <bahri@bahri.info>
2023-09-14 11:01:24 +0300

    gezegen

--- update-page.sh
+++ update-page.sh
@@ -5,7 +5,7 @@ PLANETPLANET_DIRECTORY="/planetplanet"
 cd $PLANETPLANET_DIRECTORY
 
 exec python2.7 $PLANETPLANET_DIRECTORY/planet.py $PLANETPLANET_DIRECTORY/gezegen/config.ini
-#exec python2.7 $PLANETPLANET_DIRECTORY/planet.py $PLANETPLANET_DIRECTORY/gezegen-lkd/config.ini
-#exec python2.7 $PLANETPLANET_DIRECTORY/planet.py $PLANETPLANET_DIRECTORY/gezegen-topluluk/config.ini
-#exec python2.7 $PLANETPLANET_DIRECTORY/planet.py $PLANETPLANET_DIRECTORY/gezegen/config.ini
-#exec python2.7 $PLANETPLANET_DIRECTORY/planet.py $PLANETPLANET_DIRECTORY/gezegen/config.ini
+exec python2.7 $PLANETPLANET_DIRECTORY/planet.py $PLANETPLANET_DIRECTORY/gezegen-lkd/config.ini
+exec python2.7 $PLANETPLANET_DIRECTORY/planet.py $PLANETPLANET_DIRECTORY/gezegen-topluluk/config.ini
+exec python2.7 $PLANETPLANET_DIRECTORY/planet.py $PLANETPLANET_DIRECTORY/gezegen-kisisel/config.ini
+exec python2.7 $PLANETPLANET_DIRECTORY/planet.py $PLANETPLANET_DIRECTORY/gezegen-planet/config.ini
